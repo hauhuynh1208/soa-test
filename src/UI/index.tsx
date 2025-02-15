2b4a4a242f0045dd3034ec96e47bfb50d05453c9
hauhuynh1208 <hauhuynhvnn@gmail.com>
2025-02-15 18:49:39 +0700

    Updates

--- src/UI/index.tsx
+++
@@ -1,4 +0,0 @@
-export * from "./Buttons";
-export * from "./Typography";
-export * from "./Divider";
-export * from "./Pin";
--- src/assets/index.tsx
+++
@@ -1,5 +0,0 @@
-export * from "./MountainsSVG";
-export * from "./FishingSVG";
-export * from "./CrosshairSVG";
-export * from "./MenuSVG";
-export * from "./ChatSVG";
